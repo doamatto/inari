6a2f8175b60eb38354591fa9e01811c3e5bd08cb
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-08-11 09:07:07 -0700

    chore: rewrote for Gzip

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"compress/flate"
-	"compress/gzip"
-	"io/ioutil"
 	"log"
+	"mime"
 	"net/http"
 	"os"
 	"regexp"
 	"strings"
-	"time"
+
+	"github.com/NYTimes/gziphandler"
 )
 
 func main() {
@@ -22,119 +20,67 @@ func main() {
 		port = ":8442"
 	}
 
-	// Establish endpoint for files
-	http.HandleFunc("/", router)
-
 	// Configure and start server
 	s := &http.Server{
 		Addr: port,
 	}
-	log.Printf("Inari is ready. Listening on port " + port + ".")
-	log.Fatal(s.ListenAndServe())
-}
 
-func router(w http.ResponseWriter, r *http.Request) {
 	// Fetch envvars and set defaults
 	cacheImageTime := os.Getenv("CACHE_IMAGE_TIME")
 	cacheFontTime := os.Getenv("CACHE_FONT_TIME")
 	webRoot := os.Getenv("DIRECTORY")
 	if webRoot == "" {
-		a, err := os.Getwd()
-		if err != nil {
-			log.Fatal("Couldn't fetch directory. More information may follow: ", err)
-		}
-		webRoot = a // Work around due to needing to define `err`
+		log.Printf("Using default directory")
+		webRoot = "."
 	}
 	if cacheImageTime == "" {
 		cacheImageTime = "max-age=7200"
 	} else {
+		log.Printf("Using custom image cache time")
 		cacheImageTime = "max-age=" + cacheImageTime
 	}
 	if cacheFontTime == "" {
 		cacheFontTime = "max-age=7200"
 	} else {
+		log.Printf("Using custom font cache time")
 		cacheFontTime = "max-age=" + cacheFontTime
 	}
 
-	// Add HTTP headers
-	var images = regexp.MustCompile(".(png)|.(jpg)|.(webp)|.(gif)|.(svg)")
-	switch {
-	case images.MatchString(r.URL.Path):
-		// Set headers
-		defaultHeads(w)
-		w.Header().Set("Cache-Control", cacheImageTime)
-	default:
-		// Serve with default headers
-		defaultHeads(w)
-	}
-
-	// Compress content and serve
-	acceptEncoding := r.Header.Get("Accept-Encoding")
-	if strings.Contains(acceptEncoding, "gzip") {
-		// Set headers
-		w.Header().Set("Content-Encoding", "gzip")
-
-		// Compress data
-		name := webRoot + r.URL.Path
-		file, err := os.Open(name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		data, err := ioutil.ReadAll(file)
-		if err != nil {
-			log.Fatal(err)
+	// Establish endpoint for files and use Gzip
+	s.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add HTTP headers
+		var images = regexp.MustCompile(".(png)|.(jpg)|.(webp)|.(gif)|.(svg)")
+		var fonts = regexp.MustCompile(".(ttf)|.(otf)|.(woff)|.(woff2)|.(eot)")
+		if images.MatchString(r.URL.Path) {
+			// Set headers
+			defaultHeads(w)
+			w.Header().Set("Cache-Control", cacheImageTime)
+		} else if fonts.MatchString(r.URL.Path) {
+			// Set headers
+			defaultHeads(w)
+			w.Header().Set("Cache-Control", cacheFontTime)
+		} else {
+			// Serve with default headers
+			defaultHeads(w)
 		}
 
-		var b bytes.Buffer
-		gzipWriter := gzip.NewWriter(&b, gzip.DefaultCompression)
-		defer gzipWriter.Close()
-		if _, err := gzipWriter.Write(data); err != nil {
-			log.Fatal(err)
+		// Use GZip where possible
+		fs := http.FileServer(http.Dir(webRoot))
+		typ := mime.TypeByExtension(r.URL.Path)
+		switch {
+		case strings.HasPrefix(typ, "text/"):
+			fallthrough
+		case typ == "application/xml":
+			fallthrough
+		case typ == "":
+			fs = gziphandler.GzipHandler(fs)
 		}
 
-		// Serve compressed content
-		http.ServeContent(w, r, name, time.Now(), b.Bytes())
-		return
-	} else if strings.Contains(acceptEncoding, "defalte") {
-		// Set headers
-		w.Header().Set("Content-Encoding", "deflate")
+		fs.ServeHTTP(w, r)
+	})
 
-		// Compress data
-		name := webRoot + r.URL.Path
-		file, err := os.Open(name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		data, err := ioutil.ReadAll(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var b bytes.Buffer
-		flateWriter, _ := flate.NewWriter(&b, flate.DefaultCompression)
-		defer flateWriter.Close()
-		if _, err := flateWriter.Write(data); err != nil {
-			log.Fatal(err)
-		}
-
-		// Serve compressed content
-		http.ServeContent(w, r, name, time.Now(), b.Bytes())
-		return
-	} else {
-		// Prepare files
-		name := webRoot + r.URL.Path
-		file, err := os.Open(name)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Panicf("Unable to read file to display. More info may follow: ", err)
-			return
-		}
-		defer file.Close()
-
-		// Serve content
-		http.ServeContent(w, r, name, time.Now(), file)
-		return
-	}
+	log.Printf("Inari is ready. Listening on port " + port + ".")
+	log.Fatal(s.ListenAndServe())
 }
 
 func defaultHeads(w http.ResponseWriter) {
